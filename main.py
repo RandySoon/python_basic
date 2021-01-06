24bf39d861145b2131f483ab4abd855db9beea36
RandySoon <77041346+RandySoon@users.noreply.github.com>
2021-01-06 15:29:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("Hello World!")
-
+print("Goodbye World!")
 ![Image](https://images.unsplash.com/photo-1609794203030-ae0f1dac40f7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)
